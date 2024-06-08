693e7c893659d9995831a5a37cdac0a1e2062d53
hiteshNath-Dev <hiteshsysorex@gmail.com>
2024-06-09 00:10:40 +0530

    supabase initiated

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = "https://voycetipaopnoapohszm.supabase.co";
+  static const supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZveWNldGlwYW9wbm9hcG9oc3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4NzEyMjYsImV4cCI6MjAzMzQ0NzIyNn0.99p17hj4Z4I36ytAx2qM0TK-Y0Ng73UbfdAF3LjXTKU";
+}
