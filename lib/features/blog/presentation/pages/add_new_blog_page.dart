a3fe2a0c6dc90fd01a882f8951a752a411263e7d
hiteshNath-Dev <hiteshsysorex@gmail.com>
2024-06-23 20:44:09 +0530

    screen designing

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,3 +1,5 @@
+import 'package:blog_bloc_app/core/theme/palette.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddNewBlogPage extends StatefulWidget {
@@ -19,6 +21,39 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.done_rounded))
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            DottedBorder(
+                color: AppPalette.borderColor,
+                radius: const Radius.circular(10),
+                borderType: BorderType.RRect,
+                strokeCap: StrokeCap.round,
+                dashPattern: const [10, 4],
+                child: const SizedBox(
+                  height: 150,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.folder_open,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Select your image",
+                        style: TextStyle(fontSize: 15),
+                      )
+                    ],
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,4 +1,5 @@
 import 'package:blog_bloc_app/features/blog/presentation/pages/add_new_blog_page.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
