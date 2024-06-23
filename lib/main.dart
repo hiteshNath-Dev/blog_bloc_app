5b8c052ed045c99a3462a409273f951d264355fd
hiteshNath-Dev <hiteshsysorex@gmail.com>
2024-06-23 18:34:02 +0530

    blog page and add new blog page structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:blog_bloc_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:blog_bloc_app/core/theme/theme.dart';
 import 'package:blog_bloc_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_bloc_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blog_bloc_app/features/blog/presentation/pages/blog_page.dart';
 import 'package:blog_bloc_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -44,7 +45,9 @@ class _MyAppState extends State<MyApp> {
           return state is AppUserLoggedIn;
         },
         builder: (context, isLoggedIn) {
-          if (isLoggedIn) {}
+          if (isLoggedIn) {
+            return const BlogPage();
+          }
 
           return const LoginPage();
         },
